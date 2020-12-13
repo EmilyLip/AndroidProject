ca44235c9b6efd4eb35b989222fca0df109835d3
EmilyLip <mily7975@gmail.com>
2020-12-13 18:14:03 +0200

    add click listener on login button

--- Plantarium/app/src/main/java/com/example/plantarium/MainActivity.java
+++ Plantarium/app/src/main/java/com/example/plantarium/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.plantarium;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d("TAG","button click");
     }
 }
