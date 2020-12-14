0329e75fdca8a1d904efaa2c725137f725f02ab9
EmilyLip <mily7975@gmail.com>
2020-12-14 11:12:10 +0200

    add google sign in functions

--- Plantarium/app/src/main/java/com/example/plantarium/MainActivity.java
+++ Plantarium/app/src/main/java/com/example/plantarium/MainActivity.java
@@ -1,29 +1,60 @@
 package com.example.plantarium;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 007;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SignInButton signInButton = (SignInButton) findViewById(R.id.login_button);
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton = (SignInButton) findViewById(R.id.login_button);
         TextView textView = (TextView) signInButton.getChildAt(0);
         textView.setText("התחברות עם חשבון גוגל");
+        findViewById(R.id.login_button).setOnClickListener((View.OnClickListener) this);
+
     }
 
     @Override
-    public void onClick(View view) {
-        Log.d("TAG","button click");
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                signIn();
+                break;
+            // ...
+        }
     }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 }
