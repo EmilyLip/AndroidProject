71727d658afd6417b8c0a3131bf13ea371628ad0
EmilyLip <mily7975@gmail.com>
2021-03-03 21:10:34 +0200

    try

--- Plantarium/app/src/main/java/com/example/plantarium/AddPlaceFragment.java
+++ Plantarium/app/src/main/java/com/example/plantarium/AddPlaceFragment.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
@@ -47,6 +46,4 @@ public class AddPlaceFragment extends Fragment {
             // display error state to the user
         }
     }
-
-
 }
