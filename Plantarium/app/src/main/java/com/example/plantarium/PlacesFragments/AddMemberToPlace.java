f22a57f283cbd9d95f6ec2cc74db2a652c02e46b
EmilyLip <emily7975@gmail.com>
2021-03-13 19:33:25 +0200

    send email via intent

--- Plantarium/app/src/main/java/com/example/plantarium/PlacesFragments/AddMemberToPlace.java
+++ Plantarium/app/src/main/java/com/example/plantarium/PlacesFragments/AddMemberToPlace.java
@@ -1,5 +1,6 @@
 package com.example.plantarium.PlacesFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.plantarium.Models.Place;
@@ -17,6 +19,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AddMemberToPlace extends Fragment {
     View view;
+    TextView placeName;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -24,7 +27,8 @@ public class AddMemberToPlace extends Fragment {
         view = inflater.inflate(R.layout.fragment_add_member_to_place, container, false);
         Place place = AddMemberToPlaceArgs.fromBundle(getArguments()).getPlace();
         CircleImageView placeImage = view.findViewById(R.id.place_image);
-        TextView placeName = view.findViewById(R.id.place_name);
+        placeName = view.findViewById(R.id.place_name);
+        Button inviteMemberBtn = view.findViewById(R.id.sendInviteBtn);
 
         placeName.setText(place.getName());
         placeImage.setVisibility(View.INVISIBLE);
@@ -32,6 +36,25 @@ public class AddMemberToPlace extends Fragment {
             Picasso.get().load(place.getImageUrl()).into(placeImage);
             placeImage.setVisibility(View.VISIBLE);
         }
+
+        inviteMemberBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String to= "emily7975@gmail.com";
+                String subject="נוספת למקום חדש!";
+                String message="תצטרף לאפליקציית plantarium ";
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{ to});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+
+                //need this to prompts email client only
+                email.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+            }
+        });
 
         return view;
     }
